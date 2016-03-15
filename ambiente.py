e4bd873e9514dd75347edc7759661f3031e44074
Lucas Bellomo <lbellomo@gmail.com>
2016-03-15 09:34:00 -0300

    Agregados doctrins de algunos metodos

--- ambiente.py
+++ ambiente.py
@@ -13,22 +13,36 @@ class Ambiente():
         self.salida = salida
         self.tamano_x = tamano_x
         self.tamano_y = tamano_y
-        self.obstaculos = obstaculos
-        self.robots = robots
+        self.obstaculos = obstaculos #Hacen falta los obtaculos?
+        self.robot = robot
+        self.generar_ambiente((self.tamano_x, self.tamano_y),
+                              self.entrada, self.salida)
         pass
 
-    def generar_ambiente(self):
+        def generar_ambiente(self, dimenciones, entrada, salida,
+                             algoritmo_de_generacion):
         '''
+        Genera donde se va a guardar la ubicacion de los
+        obstaculos y del robot (posiblemente una matriz de tamano
+        dimenciones, donde entrada y salida tienen la forma [x1, y1] ambos)
+        Se podria tener varios algoritmos o templates para usar.
         '''
         pass
 
     def chequear_solucion(self):
         '''
+        Comprueba que tenga solucion
+        TODO: Poder chequear cosas particulares, la
+        existencia de varios caminos por ejemplo,
+        la dificultad del laberinto.
         '''
         pass
 
     def visualizar(self):
         '''
+        TODO: hacer visualizacion
+        hacer gif de la visualizacion
+        visualizar en paralaro (multithreding)
         '''
         pass
 
---
+++ estrategia.py
@@ -0,0 +1,11 @@
+'''
+
+'''
+
+class Estrategia():
+    def __init__(self):
+        pass
+
+    def estrategia(self):
+        pass
+            
